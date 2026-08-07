89e063fe45b105cdff7f60131d367c073fa5bfc1
smithvr81-cmd <smithvr81@gmail.com>
2026-08-06 21:39:07 -0500

    Agrega app/sitemap.js para despliegue

---
+++ app/sitemap.js
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [{
+    url: 'https://www.tiendanovamovil.com',
+    lastModified: new Date(),
+    changeFrequency: 'weekly',
+    priority: 1
+  }];
+}
